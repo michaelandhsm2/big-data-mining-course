b84479416fc1708249c77615898b70ad43d2112a
Michael Fu <michaelandhsm2@gmail.com>
2018-03-28 15:06:37 +0800

    HW2 Task 1 Pass

--- hw2/sbt/src/main/scala/hw2.scala
+++ hw2/sbt/src/main/scala/hw2.scala
@@ -2,21 +2,86 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
-import java.io.File
+import java.io.{File,PrintWriter}
 
 object Task1 {
   def main(args: Array[String]): Unit = {
-    if (args.length < 2) {
-      System.err.println("Usage: HW1 <inputFile> <outputFile>")
-      System.exit(1)
+
+    Common.checkArgs(args, 3, "HW2 <inputFile(dir or file)> <outputFile(dir)> <outputLog(.txt)>")
+
+    val spark = SparkSession.builder.appName("HW2").getOrCreate()
+
+    //Get Input Data - Dir or File
+    val inputFile = new File(args(0))
+    var inputList = List[File]()
+    if(inputFile.isDirectory){
+        inputList = inputFile.listFiles.filter(_.isFile).toList
+    }else if(inputFile.isFile){
+        inputList = List[File](inputFile)
     }
 
-    val spark = SparkSession
-      .builder
-      .appName("HW2")
-      .getOrCreate()
+    //Create Initial Tuple
+    var flattenSocialData = spark.sparkContext.emptyRDD[((String, Int), (Double, Int))]
+    inputList.foreach{ input =>
+        val data = spark.sparkContext.textFile(input.toString)
+        val header = data.first
+        val flattenData = data.filter(l => l != header).
+        flatMap{ dataString =>
+            val attr = dataString.split(",")
+            attr.zipWithIndex.
+            filter{ case (value,index) => index >= 1 }.
+            map{ case (value,index) => ((attr(0),(index-1)/3),(value.toDouble,1)) }
+        }
+        flattenSocialData = flattenSocialData.union(flattenData)
+    }
+    flattenSocialData.persist()
+
+    // Generate Popularity by Day/Hour
+    val pop_by_hour = flattenSocialData.reduceByKey{case ((ia, ib), (ja, jb)) => (ia+ja, ib+jb)}
+
+    val pop_by_day = flattenSocialData.
+        map{case((uid, hr), (sum, count)) => ((uid, hr/24), (sum/count, 1))}.
+        reduceByKey{case ((ia, ib), (ja, jb)) => (ia+ja, ib+jb)}.
+        map{case((uid, day), (sum, count)) => (uid,("Day", day, sum / count))}
+
+    val all_pop = pop_by_hour.
+        map{case((uid, hr), (sum, count)) => (uid,("Hour", hr, sum / count))}.
+        union(pop_by_day).groupByKey.sortByKey(ascending = false)
+
+    //Consentrate populatarity of each news, and transform to csv
+    val all_tuple = all_pop.map { case(uid, iterable) =>
+        val vect = iterable.toVector.sortBy { tup =>
+            (tup._1, tup._2)
+        }.map{ case (doh, no, value) => value }
+        uid+","+vect.mkString(",")
+    }
+    all_tuple.saveAsTextFile(args(1))
+
+    //Output Result
+    val writer = new PrintWriter(args(2))
+    Common.printSpark(writer, spark)
+    writer.println("Loaded Files:")
+    inputList.foreach(writer.println)
+    println("")
+
+    def printSample(data:Any, title:String, format:String){
+      writer.println(title+" Data Sample: " + format)
+      writer.print(data)
+      writer.println(format+"\n")
+    }
+
+    printSample(flattenSocialData.first(), "Loaded", "((UID, Hour), (Popularity, Count))")
+    printSample(pop_by_hour.first(), "Intermediate", "((UID, Hour), (Popularity, Count))")
+    printSample(pop_by_day.first(), "Popularity by Day", "((UID, ('Day', No., Popularity Average))")
+    printSample(all_pop.first(), "All Popularity", "(UIN, ComactBuffer(all tuples))")
+
+    val test_tuple = all_pop.first()
+    printSample(all_pop.first()._2.toVector.sortBy { tup => (tup._1, tup._2) },
+                "Sorted Values", "Vector - Day * 2, Hour * 48")
+    printSample(all_tuple.first(), "Finalized", "UID, Day 1, Day 2, Hour 1, Hour 2 ... Hour 48")
 
     spark.stop()
+    writer.close()
 
   }
 }
@@ -25,5 +90,23 @@ object Task1 {
 object Task2 {
   def main(args: Array[String]): Unit = {
 
+  }
+}
+
+object Common{
+  def checkArgs(args: Array[String], requiredArgs: Int, style: String): Unit = {
+    if (args.length < requiredArgs) {
+      System.err.println(s"\nThis program expects $requiredArgs arguments.")
+      System.err.println(s"Usage: $style\n")
+      System.exit(1)
+    }
+  }
+
+  def printSpark(writer: PrintWriter, spark: SparkSession): Unit = {
+    writer.println("Spark Entity:       " + spark)
+    writer.println("Spark version:      " + spark.version)
+    writer.println("Spark master:       " + spark.sparkContext.master)
+    writer.println("Running 'locally'?: " + spark.sparkContext.isLocal)
+    writer.println("")
   }
 }
--- spark/hw_boilerplate/sbt/src/main/scala/hw2.scala
+++ spark/hw_boilerplate/sbt/src/main/scala/hw2.scala
@@ -2,96 +2,45 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
-import java.io.File
+import java.io.{File,PrintWriter}
 
-object HW1 {
+object Task1 {
   def main(args: Array[String]): Unit = {
-    if (args.length < 2) {
-      System.err.println("Usage: HW1 <inputFile> <outputFile>")
-      System.exit(1)
-    }
 
-    val spark = SparkSession
-      .builder
-      .appName("HW1")
-      .getOrCreate()
+    Common.checkArgs(args, 3, "HW2 <inputFile> <outputFile> <outputLog>")
 
-    import spark.implicits._
-
-    def sparkPrint():Unit = {
-      println("Spark Entity:       " + spark)
-      println("Spark version:      " + spark.version)
-      println("Spark master:       " + spark.sparkContext.master)
-      println("Running 'locally'?: " + spark.sparkContext.isLocal)
-      println("\n")
-    }
-    sparkPrint()
-
-    // Load File
-    val data = spark.sparkContext.textFile(args(0))
-
-
-    // Remove Header
-    val header = data.first
-    val rows = data.filter(l => l != header)
-    val hArray = Array("Date", "Time", "Active_Power", "Reactive_Power","Voltage","Intensity")
-
-    //Flatten Date
-    val flattenData = rows.
-        flatMap{ dataString =>
-            dataString.split(";").
-                zipWithIndex.
-                filter{
-                    case (value,index) => index >= 2 && index <= 5 && value !="?"
-                }.map{
-                    case (value,index) => (hArray(index),value.toDouble)
-                }
-        }
-
-    // Task 1 - Find Min, Max & Count
-    val count = flattenData.map{case (k,v) => (k,1)}.reduceByKey((i, j) => i+j).collectAsMap()
-    val max = flattenData.reduceByKey{(i, j) => if (i>j) i else j}.collectAsMap()
-    val min = flattenData.reduceByKey{(i, j) => if (i<j) i else j}.collectAsMap()
-
-    //Task 2 - Find Average
-    val average = flattenData.reduceByKey((i, j) => i+j).map{case (i, j) => (i,j/2049280)}.collectAsMap()
-    val std = flattenData.
-        map{case (k,v) => (k, scala.math.pow(v-average(k),2))}.
-        reduceByKey((i,j) => i+j).
-        map{case (k,v) => (k, math.sqrt(v/count(k)))}.collectAsMap()
-
-    //Task 3 - Normalize Data
-    val dataDF = rows.
-                    map(_.split(";")).
-                    map(att => (att(0), att(1),
-                    if(att(2)=="?") Double.NaN else (att(2).toDouble-min(hArray(2)))/(max(hArray(2))-min(hArray(2))),
-                    if(att(3)=="?") Double.NaN else (att(3).toDouble-min(hArray(3)))/(max(hArray(3))-min(hArray(3))),
-                    if(att(4)=="?") Double.NaN else (att(4).toDouble-min(hArray(4)))/(max(hArray(4))-min(hArray(4))),
-                    if(att(5)=="?") Double.NaN else (att(5).toDouble-min(hArray(5)))/(max(hArray(5))-min(hArray(5)))
-                    )).
-                    toDF(hArray(0),hArray(1),hArray(2),hArray(3),hArray(4),hArray(5))
-    dataDF.write.csv(args(1))
-    dataDF.createOrReplaceTempView("records")
+    val spark = SparkSession.builder.appName("HW2").getOrCreate()
 
     //Output Result
-    def myprint(s: String): Unit = {
-        println("For "+s+":")
-        println("        Number of Meaningful Data - " + count(s))
-        println("        Maximum Value - " + max(s))
-        println("        Minimum Value - " + min(s))
-        println("        Mean - " + average(s))
-        println("        Standard Deviation - " + std(s) + "\n")
-    }
-    sparkPrint()
-
-    myprint(hArray(2))
-    myprint(hArray(3))
-    myprint(hArray(4))
-    myprint(hArray(5))
-    println("\nNormalized Output (Selected Examples)\n")
-    spark.sql("SELECT * FROM records WHERE date = '28/4/2007'").show(25)
+    val writer = new PrintWriter(args(2))
+    Common.printSpark(writer, spark)
 
     spark.stop()
+    writer.close()
 
+  }
+}
+
+
+object Task2 {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
+
+object Common{
+  def checkArgs(args: Array[String], requiredArgs: Int, style: String): Unit = {
+    if (args.length < requiredArgs) {
+      System.err.println(s"\nThis program expects $requiredArgs arguments.")
+      System.err.println(s"Usage: $style\n")
+      System.exit(1)
+    }
+  }
+  def printSpark(writer: PrintWriter, spark: SparkSession): Unit = {
+    writer.println("Spark Entity:       " + spark)
+    writer.println("Spark version:      " + spark.version)
+    writer.println("Spark master:       " + spark.sparkContext.master)
+    writer.println("Running 'locally'?: " + spark.sparkContext.isLocal)
+    writer.println("")
   }
 }
